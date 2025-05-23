902703c482f1eecb9178b653c5d92414032d4582
LuWroblewski <wrolucas@gmail.com>
2025-05-23 00:22:24 -0300

    create navbar and add to layout.tsx

---
+++ src/app/components/layout-components/navbar/navbar.tsx
@@ -0,0 +1,76 @@
+import { LuAlignJustify, LuLanguages, LuSearch } from 'react-icons/lu';
+
+export default function Navbar() {
+  return (
+    <div className='navbar bg-base-100 shadow-sm text-[#4C4D4C] px-13'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+            <LuAlignJustify />
+          </div>
+          <ul tabIndex={0} className='menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow'>
+            <li>
+              <a>Produtos</a>
+            </li>
+            <li>
+              <a>Sobre Nós</a>
+            </li>
+            <li>
+              <a>Contato</a>
+            </li>
+            <li>
+              <a>Catálogos</a>
+            </li>
+            <li>
+              <a>
+                <LuSearch />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <a className='btn btn-ghost text-3xl tracking-widest'>LOGO</a>
+      </div>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal px-1 flex justify-center items-center'>
+          <li>
+            <a>Produtos</a>
+          </li>
+          <li>
+            <a>Sobre Nós</a>
+          </li>
+          <li>
+            <a>Contato</a>
+          </li>
+          <li>
+            <a>Catálogos</a>
+          </li>
+          <li>
+            <a>
+              <LuSearch className='text-xl' />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className='navbar-end'>
+        <a className='btn text-base-100 bg-[#E57200] rounded-xl'>Fale conosco</a>
+        <ul className='menu lg:menu-horizontal bg-ghost rounded-box'>
+          <li>
+            <details>
+              <summary className='flex items-center gap-2'>
+                <LuLanguages className='text-xl' />
+              </summary>
+              <ul>
+                <li>
+                  <button type='button'>Português</button>
+                </li>
+                <li>
+                  <button type='button'>English</button>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
