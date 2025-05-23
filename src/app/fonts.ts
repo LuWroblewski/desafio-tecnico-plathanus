f54c16d175140e85bcfb48d27dbfa768799b003b
LuWroblewski <wrolucas@gmail.com>
2025-05-22 23:47:08 -0300

    add fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Raleway, Roboto } from 'next/font/google';
+
+export const raleway = Raleway({
+  subsets: ['latin'],
+  variable: '--font-raleway',
+});
+
+export const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+});
