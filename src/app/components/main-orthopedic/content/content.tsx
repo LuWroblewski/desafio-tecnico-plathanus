6c421dd5ed1527e5cc317cd8f32370b30f58334a
LuWroblewski <wrolucas@gmail.com>
2025-05-24 13:25:14 -0300

    add link

--- src/app/components/main-orthopedic/content/content.tsx
+++ src/app/components/main-orthopedic/content/content.tsx
@@ -1,23 +1,26 @@
 import { LuChevronLeft, LuChevronRight } from 'react-icons/lu';
 import data from '../../../../data.json';
+import Link from 'next/link';
 
 export default function Content() {
   return (
     <div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:p-4'>
         {data.produtos.map((produto, index) => (
-          <div key={index} className='p-4 shadow hover:shadow-md transition duration-300 relative'>
-            <div className='indicator w-auto'>
-              {produto.lancamento && (
-                <span className='indicator-item indicator-start ml-15 badge-xl badge bg-[#9A1A8099] text-white'>
-                  Lançamento
-                </span>
-              )}
-              <img src={produto.imagem} alt={produto.nome} className='w-full h-auto object-cover rounded' />
+          <Link href={'/product'}>
+            <div key={index} className='p-4 shadow hover:shadow-md transition duration-300 relative'>
+              <div className='indicator w-auto'>
+                {produto.lancamento && (
+                  <span className='indicator-item indicator-start ml-15 badge-xl badge bg-[#9A1A8099] text-white'>
+                    Lançamento
+                  </span>
+                )}
+                <img src={produto.imagem} alt={produto.nome} className='w-full h-auto object-cover rounded' />
+              </div>
+              <h2 className='mt-2 text-lg font-semibold'>{produto.nome}</h2>
+              <p className='text-gray-600'>Código: {produto.codigo}</p>
             </div>
-            <h2 className='mt-2 text-lg font-semibold'>{produto.nome}</h2>
-            <p className='text-gray-600'>Código: {produto.codigo}</p>
-          </div>
+          </Link>
         ))}
       </div>
 
